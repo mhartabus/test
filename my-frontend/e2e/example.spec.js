42429585b599f2a34a82a14495367419f2b8d7c4
Mateusz Hartabus <mateusz_hartabus@trimble.com>
2024-02-07 16:00:15 +0100

    check if playwright will work with localserver

--- my-frontend/e2e/example.spec.js
+++ my-frontend/e2e/example.spec.js
@@ -2,18 +2,10 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.getByText('Learn React').click();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
