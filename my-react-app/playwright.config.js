b33a6c16e9476270fba2e8fb79daac00f057f6c1
Mateusz Hartabus <mateusz_hartabus@trimble.com>
2024-02-08 16:37:52 +0100

    added video

--- my-react-app/playwright.config.js
+++ my-react-app/playwright.config.js
@@ -28,7 +28,8 @@ module.exports = defineConfig({
     baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on-first-retry',
+    video:'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
