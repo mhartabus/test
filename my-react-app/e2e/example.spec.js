51c963442c1fb572196ed662ed00be64a13aab2b
Mateusz Hartabus <mateusz_hartabus@trimble.com>
2024-02-08 15:43:19 +0100

    updated path

--- my-react-app/e2e/example.spec.js
+++ my-react-app/e2e/example.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await page.getByText('Learn React').click();
+  await page.getByText('Learn Rect').click();
 });
 
 
